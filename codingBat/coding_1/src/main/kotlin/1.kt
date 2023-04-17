353746343d01668757d4e0bdf843a7f1ec932137
josip samardzic <josip.samardzic2@gmail.com>
2023-04-17 03:15:23 +0200

    codingBat

--- codingBat/coding_1/src/main/kotlin/1.kt
+++ codingBat/coding_1/src/main/kotlin/1.kt
@@ -1,9 +1,36 @@
+import java.util.InputMismatchException
+import java.util.Scanner
+
 fun main() {
 
-    val a = "hi there"
-    val b = "hi"
-    val c = "hello hi"
+    val scanner = Scanner(System.`in`)
+    var a = 0
+    var b: Int
 
-    if (a[0] && a[1])
+    println("Enter a num: ")
+    var a: Int
+    try {
+        var a = scanner.nextInt()
+    }catch (ex: InputMismatchException){
+        println("Must enter a number!")
+        scanner.nextLine()
+    }
+
+    println("Enter second num: ")
+    var b: Int
+    try {
+        b = scanner.nextInt()
+    }catch (ex: InputMismatchException){
+        println("Must enter a number!")
+        scanner.nextLine()
+    }
+
+    if (a == 6 || b == 6){
+        println("true")
+    }else if (a + b == 6){
+        println("true")
+    }else{
+        println("false")
+    }
 
 }
