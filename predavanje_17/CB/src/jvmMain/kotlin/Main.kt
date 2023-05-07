6cf451b115824a56a5a8d16dc9b9e64dcc3e1d91
josip samardzic <josip.samardzic2@gmail.com>
2023-05-07 19:01:05 +0200

    cb

--- predavanje_17/CB/src/jvmMain/kotlin/Main.kt
+++ predavanje_17/CB/src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import javax.swing.GroupLayout.Alignment
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.TextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 
 
 fun main() = application {
@@ -27,15 +31,40 @@ fun main() = application {
 @Composable
 @Preview
 fun App() {
-   // var text by remember { mutableStateOf("Hello, World!") }
-   // Text(text)
+    var text by remember { mutableStateOf("") }
+
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {  }
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Enter your name") }
+        )
 
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Enter your password") }
+        )
+
+        Button(
+            modifier = Modifier.background(Color.Gray),
+            onClick = {
+                println("logged in")
+                Modifier.background(color()).also {}
+            }
+        ) { Text("Log in") }
+    }
 }
+
+fun color(): Color{
+    val list = listOf(Color.Black, Color.Red, Color.Green)
+    return list[(list.indices).random()]
+}
+
 
 
